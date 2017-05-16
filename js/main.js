8a8d05f0cda8ad842d5be353989055af56837137
Aleksey <u608110@gmai.com>
2017-05-16 10:04:38 +0300

    Section About

--- js/main.js
+++ js/main.js
@@ -5,5 +5,16 @@ $(function () {
         delay: 5000,
         arrows: false
     });
+
+    // Выравнивание описание товара в карточке
+    function setEqualHeight(columns) {
+        var tallestcolumn = 0;
+        columns.each( function() {
+            currentHeight = $(this).height();
+            if(currentHeight > tallestcolumn) { tallestcolumn = currentHeight; } } );
+        columns.height(tallestcolumn);
+    }
+
+    $(document).ready(function() { setEqualHeight($(".products-text")); });
 });
 
